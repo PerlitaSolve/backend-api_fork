0eeb2ddb60d677e0f7ca80566e8c2429471317c7
PerlitaSolve <124049201@upq.edu.mx>
2026-02-11 20:27:59 -0600

    Busqueda en productos y categorias

--- src/controllers/externalController.js
+++ src/controllers/externalController.js
@@ -9,7 +9,7 @@ const poblarProductos = async (request, response) => {
         let inserciones = 0;
         // Destructurar el objeto
         for(const product of products){
-            const { title, price, description, image} = product;
+            const { title, price, description, image, category} = product;
 
             const stock = Math.floor(Math.random() * 50) + 1;
 
@@ -19,7 +19,7 @@ const poblarProductos = async (request, response) => {
                 VALUES ($1, $2, $3, $4, $5)
             `
 
-            await pool.query(query, [title, price, stock, description, image]);
+            await pool.query(query, [title, price, stock, description, image, category]);
 
             inserciones++;
         }
@@ -35,4 +35,81 @@ const poblarProductos = async (request, response) => {
     }
 };
 
-module.exports = { poblarProductos };
+const anadirCategoria = async (req, res) => {
+    try{
+        const apiFech = await fetch('http://fakestoreapi.com/products');
+        const productosLista = await apiFech.json();
+        let modificados =0;
+
+        for(const producto of productosLista){
+            const categoria = producto.category;
+
+            const query = await pool.query('select id from categoria where nombre = $1', [categoria]);
+            await pool.query('update productos set id_categoria =$1 where nombre = $2', [query.rows[0].id, producto.title]);
+            modificados++;
+
+        }
+        res.status(200).json({mensaje: "Categorias añadidas exitosamente", cantidad: modificados});
+
+    }catch(error){
+        console.log(`Error: ${error}`);
+        response.status(500).json({error: error.message})
+    }
+};
+
+const poblarCategoria = async (req, res) => {
+    try {
+        // Fetch FakeStoreApi
+        const apiFetch = await fetch('http://fakestoreapi.com/products');
+        const categorias = await apiFetch.json();
+
+        let inserciones = 0;
+        // Destructurar el objeto
+        for(const categoria of categorias){
+            const {category} = categoria;
+
+
+            const query = `INSERT INTO categoria (nombre) VALUES ($1) ON CONFLICT (nombre) DO NOTHING`;
+
+            await pool.query(query, [category]);
+
+            inserciones++;
+        }
+        res.status(200).json(
+            {
+                mensaje: "Carga de categorias exitosa", 
+                cantidad: inserciones
+            }
+        );
+        
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        response.status(500).json({error: error.message})
+    }
+};
+
+const buscarProducto = async (req, res) => {
+    const Busqueda = req.params.busqueda;
+
+    try{
+        const row = await pool.query('SELECT * FROM productos where nombre ilike $1', [`%${Busqueda}%`]);
+        res.json(row.rows);
+    }catch(error){
+        console.log(`Error: ${error}`);
+        res.status(500).json({error: error.message});
+    }
+};
+
+const buscarCategoria = async (req, res) => {
+    const Busqueda = req.params.busqueda;
+
+    try{
+        const row = await pool.query('SELECT * FROM categoria where nombre ilike $1', [`%${Busqueda}%`]);
+        res.json(row.rows);
+    }catch(error){
+        console.log(`Error: ${error}`);
+        res.status(500).json({error: error.message});
+    }
+};
+
+module.exports = { poblarProductos, poblarCategoria, anadirCategoria, buscarProducto , buscarCategoria };
--- src/routes/productoRoute.js
+++ src/routes/productoRoute.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const { poblarProductos } = require('../controllers/externalController');
+const {poblarProductos} = require('../controllers/externalController');
+const {poblarCategoria} = require('../controllers/externalController');
+const {anadirCategoria} = require('../controllers/externalController');
+const {buscarProducto}= require('../controllers/externalController');
+const {buscarCategoria} = require('../controllers/externalController');
 const router = express.Router();
 
-router.post('/poblar', poblarProductos);
+router.post('/poblarProducts', poblarProductos);
+router.post('/poblarCategoria', poblarCategoria);
+router.post('/anadirCategoria', anadirCategoria);
+router.get('/buscarProducto/:busqueda', buscarProducto);
+router.get ('/buscarCategoria/:busqueda', buscarCategoria);
+
 
 module.exports = router;
