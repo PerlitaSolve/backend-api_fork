6b0ae42cb4e4f959ce199838f73bae4db39d29e4
PerlitaSolve <124049201@upq.edu.mx>
2026-03-03 20:45:26 -0600

    crear producto

--- src/routes/productoRoute.js
+++ src/routes/productoRoute.js
@@ -5,6 +5,8 @@ const {anadirCategoria} = require('../controllers/externalController');
 const {buscarProducto}= require('../controllers/externalController');
 const {buscarCategoria} = require('../controllers/externalController');
 const {mostrar} = require('../controllers/externalController');
+const {crearProducto} = require('../controllers/externalController');
+const authMiddleware = require('../authMiddleware');
 const router = express.Router();
 
 router.post('/poblarProducts', poblarProductos);
@@ -13,6 +15,7 @@ router.post('/anadirCategoria', anadirCategoria);
 router.get('/buscarProducto/:busqueda', buscarProducto);
 router.get ('/buscarCategoria/:busqueda', buscarCategoria);
 router.get('/mostrar', mostrar);
+router.post('/crear', authMiddleware, crearProducto);
 
 
 module.exports = router;
